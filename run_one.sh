43f6e24a9145990fcb5c854e904768df3b128a2f
unknown <b05202061@ntu.edu.tw>
2020-01-14 17:58:26 +0800

    ok

--- run_one.sh
+++ run_one.sh
@@ -2,14 +2,14 @@
 DATA_DIR="data/"
 SYN_DIR="Synthetic/"
 OUT_DIR="Output/"
-i=0
+i=3
 LEFT_PATH="${DATA_DIR}${SYN_DIR}TL$i.png"
 RIGHT_PATH="${DATA_DIR}${SYN_DIR}TR$i.png"
 GT_PATH="${DATA_DIR}${SYN_DIR}TLD$i.pfm"
-OUT_PATH="${DATA_DIR}${OUT_DIR}out$i.pfm"
+OUT_PATH="${DATA_DIR}${OUT_DIR}out$i.jpg"
 touch $OUT_PATH
 # If reading from config:
-python3 main.py -c
+python main.py -c
 
 # Else specify arguments:
 # python3 main.py --input-left $LEFT_PATH --input-right $RIGHT_PATH --output $OUT_PATH --GT $GT_PATH --CM_base --OP_base --RF_base
